4f4db5b62bf7b7b4c1a4f5f71b5983c821692fdb
akki4169@gmail.com Kulkarni <akki4169@gmail.com>
2018-05-28 04:21:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var recipes=new Object();
 function updateObjectWithKeyAndValue(recipes,key,value){
  return Object.assign({}, recipes, { [key]: value })
 }
+
 function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
   return object['key'].value;
 }
